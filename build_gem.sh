4d13b60806793cdababc41210144df213c805eab
subpointsupport <support@subpointsolutions.com>
2018-06-20 01:20:38 +1000

    + testing CI, integration builds

--- build_gem.sh
+++ build_gem.sh
@@ -1,10 +1,19 @@
 
+BUILD_DIR=.build
+GEM_FOLDER="./src/metabox"
+
+GEM_MAJOR_VERSION="0.2.3"
+GEM_VERSION_FILE=./src/metabox/lib/metabox/version.rb
+
 if [[ -z $APPVEYOR_BUILD_NUMBER ]] ; then 
-    APPVEYOR_BUILD_NUMBER=1000
+    GEM_MINOR_VERSION="9999-local"
     echo 'Running locally. Set APPVEYOR_BUILD_NUMBER=$APPVEYOR_BUILD_NUMBER' 
 else 
     echo "Running under CI" ; 
+    GEM_MINOR_VERSION="${APPVEYOR_BUILD_NUMBER}-${APPVEYOR_REPO_BRANCH}"
 fi
+
+GEM_VERSION="${GEM_MAJOR_VERSION}.${GEM_MINOR_VERSION}"
 
 if [ "$1" == "--install" ] ; then 
     INSTALL_GEM=1
@@ -38,7 +47,6 @@ function cleanup_old_gems() {
 }
 
 function build_gem() {
-    
 
     echo "Building gem..."
     sh -c "cd $GEM_FOLDER && gem build *.gemspec"
@@ -56,22 +64,26 @@ function prepare_build_folder() {
     validate_exit_code $? "Cannot cleanup folder: $BUILD_DIR"
 }
 
+function get_latest_gem() {
+    if [[ -z $APPVEYOR_REPO_BRANCH ]] ; then 
+        LATEST_GEM_VERSION="latest"
+    else 
+        LATEST_GEM_VERSION="${APPVEYOR_REPO_BRANCH}-latest"
+    fi
+
+    echo "$BUILD_DIR/metabox-${LATEST_GEM_VERSION}.gem"
+}
+
 function copy_gem_to_build_folder() {
-    echo "Copying 'latest' gem to build dir: $BUILD_DIR"
-    
+    echo "Copying newly built gem to build dir: $BUILD_DIR"
     cp ./src/metabox/*.gem $BUILD_DIR/
     validate_exit_code $? "Cannot copy latest gem to folder: $BUILD_DIR"
 
-    echo "Copying 'metabox-nightly.gem' to build dir: $BUILD_DIR"
-    cp ./src/metabox/metabox-0*.gem $BUILD_DIR/metabox-nightly.gem
-    validate_exit_code $? "Cannot copy nighly gem to folder: $BUILD_DIR"
+    LATEST_GEM_PATH=$(get_latest_gem)
+
+    cp ./src/metabox/metabox-0*.gem $LATEST_GEM_PATH
+    validate_exit_code $? "Cannot copy latest gem to folder: $BUILD_DIR"
 }
-
-BUILD_DIR=.build
-GEM_FOLDER="./src/metabox"
-
-GEM_VERSION="0.2.3.${APPVEYOR_BUILD_NUMBER}"
-GEM_VERSION_FILE=./src/metabox/lib/metabox/version.rb
 
 echo "Bulding gem version: $GEM_VERSION"
 
@@ -86,12 +98,11 @@ copy_gem_to_build_folder
 
 echo "Completed build!"
 
-
 if [ $INSTALL_GEM ] ; then 
-    GEM_PATH="$BUILD_DIR/metabox-nightly.gem"
+    GEM_PATH=$(get_latest_gem)
 
-    echo "Unisntalling metabox gem -v $GEM_VERSION"
-    gem uninstall metabox -v $GEM_VERSION --force -x
+    echo "Unisntalling metabox gems"
+    gem uninstall metabox --force -x
 
     echo "Installing latest gem: $GEM_PATH"
     gem install --local $GEM_PATH --no-ri --no-rdoc --force
